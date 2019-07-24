038c21c1eb1bfdc145cb46be034590a27fbaf64d
anton_skomarovskyi <anton.skomarovskyi@7digital.com>
2019-07-24 15:28:15 +0300

    Initial Commit

---
+++ DOUCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DOUCalendar
+//
+//  Created by Anton Skomarovskyi on 7/24/19.
+//  Copyright © 2019 Anton Skomarovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
